a2523782f8ec3894f5b36361cad80fa7d7013fe0
bblks <168915058+bblks@users.noreply.github.com>
2024-08-14 10:28:44 +0300

    Update play.py

--- YousefMusic/utils/inline/play.py
+++ YousefMusic/utils/inline/play.py
@@ -1,5 +1,5 @@
 import math
-
+import config
 from YousefMusic import app 
 
 from pyrogram.types import InlineKeyboardButton
@@ -34,23 +34,41 @@ def stream_markup_timer(_, chat_id, played, dur):
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
-
+    if 0 < umm <= 10:
+        bar = "◉—————————"
+    elif 10 < umm < 20:
+        bar = "—◉————————"
+    elif 20 <= umm < 30:
+        bar = "——◉———————"
+    elif 30 <= umm < 40:
+        bar = "———◉——————"
+    elif 40 <= umm < 50:
+        bar = "————◉—————"
+    elif 50 <= umm < 60:
+        bar = "—————◉————"
+    elif 60 <= umm < 70:
+        bar = "——————◉———"
+    elif 70 <= umm < 80:
+        bar = "———————◉——"
+    elif 80 <= umm < 95:
+        bar = "————————◉—"
+    else:
+        bar = "—————————◉"
     buttons = [
                 [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
-        ],[  
-            InlineKeyboardButton(text="انهاء", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="استكمال", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="ايقاف", callback_data=f"ADMIN Pause|{chat_id}"),
-            
         ],[
-            InlineKeyboardButton(text=" 𝐬𝐨𝐮𝐫𝐜𝐞 𝐚𝐥𝐢𝐜𝐞", url=f"https://t.me/ngd_1"),
-            
+            InlineKeyboardButton(text="إستئناف ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="إيقاف مؤقت", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text=" إعاده", callback_data=f"ADMIN Replay|{chat_id}"),
         ],[
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+            InlineKeyboardButton(text=" تخطي ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text=" توقف ", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],[
+            InlineKeyboardButton(text=config.CHANNEL_NAME, url=config.CHANNEL_LINK)],
     ]
     return buttons
 
@@ -58,15 +76,14 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="انهاء", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="استكمال", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="ايقاف", callback_data=f"ADMIN Pause|{chat_id}"),
-            
+            InlineKeyboardButton(text="إستئناف ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="إيقاف مؤقت", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text=" إعاده", callback_data=f"ADMIN Replay|{chat_id}"),
         ],[
-            InlineKeyboardButton(text=" 𝐬𝐨𝐮𝐫𝐜𝐞 𝐚𝐥𝐢𝐜𝐞", url=f"https://t.me/ngd_1"),
-           
+            InlineKeyboardButton(text=" تخطي ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text=" توقف ", callback_data=f"ADMIN Stop|{chat_id}"),
         ],[
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+            InlineKeyboardButton(text=config.CHANNEL_NAME, url=config.CHANNEL_LINK)],
     ]
     return buttons
 
@@ -117,7 +134,8 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",),
+                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+            ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
                 callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
@@ -133,9 +151,9 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
                 callback_data=f"forceclose {query}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="𝐑𝐄𝐒𝐔𝐌𝐄",
+                text="𖣂 𝒓𝒆𝒔𝒖𝒎𝒆 𖣂",
                 callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
             ),
         ],
     ]
-    return buttons 
+    return buttons
