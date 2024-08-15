a9bcf556cbcd798c0489f48530c36d5e23681dfb
bblks <168915058+bblks@users.noreply.github.com>
2024-08-15 23:41:49 +0300

    Update cmds.py

--- YousefMusic/plugins/play/cmds.py
+++ YousefMusic/plugins/play/cmds.py
@@ -14,14 +14,14 @@ from YousefMusic import app
 from config import OWNER_ID, LOGGER_ID
 
 
-@app.on_message(command(["ميوزك", "الميوزك", "اوامر"]))
+@app.on_message(command(["ميوزك", "الميوزك", "التشغيل "]))
 async def zdatsr(client: Client, message: Message):
     usr = await client.get_users(OWNER_ID)
     name = usr.first_name
     usrnam = usr.username
     await message.reply_photo(
         photo=f"https://telegra.ph/file/65ae4f8eed3deb620852b.jpg",
-        caption=f"""<b>» مرحبـاً بك عـزيـزي </b> {message.from_user.mention} .\n\n<b>» استخـدم الازرار بالاسفـل \n» لـ تصفـح اوامـر سورس ⁷⁷𝒏𝒂𝒋𝒅 🇸🇦</b>""",
+        caption=f"""<b>» مرحبـاً بك عـزيـزي </b> {message.from_user.mention} .\n\n<b>» استخـدم الازرار بالاسفـل \n» لـ تصفـح اوامـر السورس </b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -32,9 +32,6 @@ async def zdatsr(client: Client, message: Message):
                         " اوامـر القنـاة ", callback_data="zzzch"),
                     InlineKeyboardButton(
                        " اوامـر الادمـن ", callback_data="zzzad"),
-                ],[ 
-                    InlineKeyboardButton(
-                        "𝐬𝐨𝐮𝐫𝐜𝐞 𝐚𝐥𝐢𝐜𝐞", url="https://t.me/ngd_1") 
                 ],[
                     InlineKeyboardButton(
                         "رجـوع", callback_data="zzzback"), 
